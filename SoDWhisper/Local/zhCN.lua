2852b318b71d6b010296fb7e85e006b91b6271e4
ShaftOfDoom <106627070+ShaftOfDoom@users.noreply.github.com>
2022-05-31 16:25:21 -0500

    Delete SoDWhisper directory

--- SoDWhisper/Local/zhCN.lua
+++
@@ -1,4 +0,0 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("SoDWhisper", "zhCN")
-if not L then return end
-
---@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
